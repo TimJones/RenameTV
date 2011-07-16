0ca7bde582574d921aa63f06a91730dfd8029d41
Tim Jones <tim@freakinabox.com>
2011-07-16 14:39:35 +0200

    Updated source documentation for episodeselector.cpp
    Signed-off-by: Tim Jones <tim@freakinabox.com>

--- episodeselector.cpp
+++ episodeselector.cpp
@@ -17,9 +17,23 @@ You should have received a copy of the GNU General Public License
 along with RenameTV.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/*! \file episodeselector.hpp
+    \brief Definition of the EpisodeSelector class
+    \file episodeselector.cpp
+    \brief Implimentation for the EpisodeSelector class
+*/
+
 #include "episodeselector.hpp"
 #include "ui_episodeselector.h"
 
+/*! \class EpisodeSelector
+    \headerfile episodeselector.hpp
+    \brief The GUI window class for selecting episode(s)
+*/
+
+/*! \brief Constructor
+    \param parent QWidget pointer to the parent QWidget of the MainWindow
+*/
 EpisodeSelector::EpisodeSelector(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::EpisodeSelector)
@@ -27,11 +41,16 @@ EpisodeSelector::EpisodeSelector(QWidget *parent) :
     ui->setupUi(this);
 }
 
+/*! \brief Destructor
+*/
 EpisodeSelector::~EpisodeSelector()
 {
     delete ui;
 }
 
+/*! \brief Adds a selectable season to the season combo box
+    \param season The numerical order of the season to be added
+*/
 void EpisodeSelector::AddSeason( int const& season )
 {
     QString seasonName = tr( "Season %1" ).arg( season );
@@ -39,26 +58,41 @@ void EpisodeSelector::AddSeason( int const& season )
     m_map.insert( seasonName, season );
 }
 
+/*! \brief Adds a selectable episode to the episode list box
+    \param episode A string representation of an episode title to be added
+*/
 void EpisodeSelector::AddEpisode( QString const& episode )
 {
     ui->listEpisodes->addItem( episode );
 }
 
+/*! \brief Checks for an entire season being selected
+    \return True if an entire season or all seasons are selected
+*/
 bool EpisodeSelector::isSeasonSelected() const
 {
     return ui->radioSeason->isChecked();
 }
 
+/*! \brief Checks for an episode or selection of episodes being selected
+    \return True if an episode or selection of episodes are selected
+*/
 bool EpisodeSelector::isEpisodeSelected() const
 {
     return ui->radioEpisode->isChecked();
 }
 
+/*! \brief Get the currently selected season
+    \return The index value of the selected season
+*/
 int EpisodeSelector::GetSelectedSeason() const
 {
     return m_map.value( ui->comboSeasons->currentText(), -1 );
 }
 
+/*! \brief Checks for an episode or selection of episodes being selected
+    \return A list of QStrings of episode titles selected
+*/
 QStringList EpisodeSelector::GetSelectedEpisodes() const
 {
     QStringList list;
