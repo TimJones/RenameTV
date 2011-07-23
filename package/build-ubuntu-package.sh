89f424511259a9df86035f638844ba311c274cd9
Tim Jones <tim@freakinabox.com>
2011-07-23 09:20:52 +0200

    Build package script now makes a fully clean source-only orig.tar.gz file
    Signed-off-by: Tim Jones <tim@freakinabox.com>

--- package/build-ubuntu-package.sh
+++ package/build-ubuntu-package.sh
@@ -11,13 +11,16 @@ ln -snf $SOURCE_DIR/ $PROJECT-$VERSION
 
 # Create the orig tarball, excluding the pwd (within the symlink) so we don't
 #  get circular references
-tar --exclude-vcs --exclude --exclude "$PROJECT-$VERSION/build/*" --exclude "$PROJECT-$VERSION/build" --exclude "$PROJECT-$VERSION/${PWD##*/}/*" --exclude "$PROJECT-$VERSION/${PWD##*/}" --exclude "$PROJECT-$VERSION/*.pro.user" -czhvf ${PROJECT}_${VERSION}.orig.tar.gz $PROJECT-$VERSION/
+tar --exclude-vcs --exclude "$PROJECT-$VERSION/debian/*" --exclude "$PROJECT-$VERSION/debian" --exclude "$PROJECT-$VERSION/build/*" --exclude "$PROJECT-$VERSION/build" --exclude "$PROJECT-$VERSION/${PWD##*/}/*" --exclude "$PROJECT-$VERSION/${PWD##*/}" --exclude "$PROJECT-$VERSION/*.pro.user" -czhvf ${PROJECT}_${VERSION}.orig.tar.gz $PROJECT-$VERSION/
 
 # Remove the symlink and extract the tarball here
 #   - debuild will follow the symlink and output it's files in the parent of the 
 #      source dir, rather than in the pwd as we need
 rm $PROJECT-$VERSION
 tar -xvf ${PROJECT}_${VERSION}.orig.tar.gz
+
+# Copy debian package folder into the clean source folder
+cp -r $SOURCE_DIR/debian $PROJECT-$VERSION/debian
 
 # Create a copy of the debian changelog file for our editing
 cp $PROJECT-$VERSION/debian/changelog ./changelog.orig
